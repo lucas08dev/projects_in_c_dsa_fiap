49c7bf402a550f11ebef4b43caec6258f13e841b
Lucas <lucas.sabreu2008@gmail.com>
2026-03-21 15:30:14 -0300

    desenvolvendo linguagem C

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Integrantes do grupo:\n");
+    printf("Lucas Silva de Abreu\n");
+    printf("Nicolas Castana Nishi\n");
+    printf("Enzo Giovani Guislandi\n");
+    printf("Guilherme Augusto da Silva Reiche\n");
+    printf("João Pedro Camperlingo"); 
+
+    return 0;
+}
